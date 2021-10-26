4291ad5560e683f74cebc35b674edb549e0f647a
Kirk Martinez <km@ecs.soton.ac.uk>
2021-10-26 10:56:50 +0100

    import this for easier rgb led

--- myled.py
+++ myled.py
@@ -1,14 +1,12 @@
 # handy functions for RGB LED
-# not perfect yet
-import time
+# usage: from myled import rgbled then make something like led = rgbled()
 import board
 import adafruit_dotstar
-
 
 class rgbled(object):
     def __init__(self):
         self.led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
-        self.led.brightness = 0.5
+        self.led.brightness = 0.3
     def flash(self):
         self.led.brightness = 1
         time.sleep(0.5)
@@ -19,16 +17,9 @@ class rgbled(object):
         self.led[0] = (0, 255, 0)
     def blue(self):
         self.led[0] = (0, 0, 255)
+    def red(self):
+        self.led[0] = (200, 0, 0)
+    def off(self):
+        self.led[0] = (0,0,0)
 
 
-myled = rgbled()
-myled.blue()
-print("blue flash")
-myled.flash()
-myled.green()
-print("green flash")
-myled.flash()
-while True:
-    myled.brightness(0)
-    time.sleep(1)
-    myled.brightness(1)
