b3bd0407bd6428b4dde3c5e9eb96585c463b6a9f
Kirk Martinez <km@ecs.soton.ac.uk>
2021-11-11 10:12:19 +0000

    simplfied to find mac then test wifi

--- getMAC.py
+++ getMAC.py
@@ -1,4 +1,6 @@
-# try wifi, get mac
+# get our Wifi MAC address (for registering)
+# put ssid and pass in your secrets.py
+# uncomment the testwifi() to check it works
 
 import wifi
 def formatmac(mac):
@@ -8,20 +10,22 @@ def formatmac(mac):
         smac += h[2] + h[3]
     return(smac)
 
-# Get wifi secrets
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets in secrets.py, add them there!")
-    raise
 
-wifi.radio.connect(secrets["ssid"], secrets["password"])
 
-print("My IP address is", wifi.radio.ipv4_address)
+def testwifi():
+    # Get wifi secrets
+    try:
+        from secrets import secrets
+        print("connecting to ",secrets["ssid"] )
+        wifi.radio.connect(secrets["ssid"], secrets["password"])
+        print("My IP address is", wifi.radio.ipv4_address)
+    except ImportError:
+        print("WiFi secrets needed in secrets.py, add them there")
+        raise
+
+
 mac = wifi.radio.mac_address
-#smac = ""
-#for ot in list(mac):
-#    h = hex(ot)
-#    smac += h[2] + h[3]
 smac = formatmac(mac)
 print("MAC: ", smac)
+# uncomment once you have put things in secrets.py
+#testwifi()
