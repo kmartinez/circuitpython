4bac3f6c5171aaf268b74d4bfb2efd9eb0d3133c
Kirk Martinez <km@ecs.soton.ac.uk>
2022-10-20 10:10:29 +0100

    remove unused line

--- simpleLED.py
+++ simpleLED.py
@@ -1,11 +1,11 @@
-# how to use featherS2 -  onboard blue LED
+# how to use featherS2 -  flash the onboard blue LED
 import time
 import board
 import digitalio
 
 blueled = digitalio.DigitalInOut(board.LED)
 blueled.direction = digitalio.Direction.OUTPUT
-c = 3
+
 while True:
     blueled.value = True
     time.sleep(0.5)
