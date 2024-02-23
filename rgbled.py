5caa352b96f8e0114bfa81cea6da9db4807b056e
Kirk Martinez <km@ecs.soton.ac.uk>
2024-02-23 13:02:32 +0000

    Update rgbled.py duel version for neopixel and apa102

--- rgbled.py
+++ rgbled.py
@@ -1,10 +1,13 @@
-# how to use featherS2 onboard RGB LED
+# how to use onboard RGB LED
 # the led values are (red,green,blue)
+# check if you have a neopixel (like on S3) or APA102 (like on S2)
 from time import sleep
 import board
 import adafruit_dotstar
 
 led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
+# version for Feather S3
+#led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 led.brightness = 0.2
     
 while True:
