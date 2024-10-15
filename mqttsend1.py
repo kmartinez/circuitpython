45fbd1d2f1d6f4739b5a0f87e76c1fabe660a5fd
Kirk Martinez <km@ecs.soton.ac.uk>
2024-10-15 14:22:32 +0100

    Update mqttsend1.py
    updated to newer examples

--- mqttsend1.py
+++ mqttsend1.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
-# Run this on Win10/Linux/Mac to send a message to the MQTT broker
-# change the publish topic to your own message
+# Run this to send a message to a MQTT broker
+# change the publish topic to your own message eg username/
+# adapted from paho-mqtt publisher example
+# you need pip3 install paho-mqtt
 import paho.mqtt.client as mqtt
+import time
 
 broker = "srv03183.soton.ac.uk"
-port = 1883
 
-def on_publish(client,userdata,result):             #create function for callback
-    print("data published")
-    pass
-    
-client1= mqtt.Client("control1")                           #create client object
-client1.on_publish = on_publish                          #assign function to callback
-client1.connect(broker,port)                                 #establish connection
-ret= client1.publish("house/switch","on") 
+def on_publish(client, userdata, mid, reason_code, properties):
+    try:
+        userdata.remove(mid)
+    except KeyError:
+        print("some error ocurred")
+
+unacked_publish = set()
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_publish = on_publish
+
+mqttc.user_data_set(unacked_publish)
+mqttc.connect(broker)
+mqttc.loop_start()
+
+# Our application produce some messages
+msg_info = mqttc.publish("username/topic", "a test message", qos=1)
+unacked_publish.add(msg_info.mid)
+
+# Wait for all message to be published
+while len(unacked_publish):
+    time.sleep(0.1)
+
+# Due to race-condition the following way to wait for all publish is safer
+msg_info.wait_for_publish()
+
+mqttc.disconnect()
+mqttc.loop_stop()
